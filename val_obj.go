357ac9ffa8f620ab60775168c76c655bfb62983a
yiğit oztemel <yigit.oztemel@beyn.com.tr>
2017-06-09 12:58:04 +0300

    int types added

--- val_obj.go
+++ val_obj.go
@@ -21,3 +21,17 @@ func (v *Valobj) String() string {
 func (v *Valobj) Int() int {
 	return v.Get().(int)
 }
+
+func (v *Valobj) Int8() int8 {
+	return v.Get().(int8)
+}
+
+func (v *Valobj) Int16() int16 {
+	return v.Get().(int16)
+}
+func (v *Valobj) Int32() int32 {
+	return v.Get().(int32)
+}
+func (v *Valobj) Int64() int64 {
+	return v.Get().(int64)
+}
--- val_obj_test.go
+++ val_obj_test.go
@@ -2,6 +2,7 @@ package valobj_test
 
 import (
 	"testing"
+
 	"github.com/ygto/value-object"
 )
 
@@ -16,6 +17,38 @@ func TestInt(t *testing.T) {
 	age := 50
 	data := valobj.Val(age)
 	if data.Int() != age {
+		t.Fail()
+	}
+}
+func TestInt8(t *testing.T) {
+	var age int8
+	age = 50
+	data := valobj.Val(age)
+	if data.Int8() != age {
+		t.Fail()
+	}
+}
+func TestInt16(t *testing.T) {
+	var age int16
+	age = 50
+	data := valobj.Val(age)
+	if data.Int16() != age {
+		t.Fail()
+	}
+}
+func TestInt32(t *testing.T) {
+	var age int32
+	age = 50
+	data := valobj.Val(age)
+	if data.Int32() != age {
+		t.Fail()
+	}
+}
+func TestInt64(t *testing.T) {
+	var age int64
+	age = 50
+	data := valobj.Val(age)
+	if data.Int64() != age {
 		t.Fail()
 	}
 }
